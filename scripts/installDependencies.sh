6233f47749a5ad98a9852b48ff966a952c3da0ce
Ivan Moreno <ivan1697@gmail.com>
2019-07-02 01:20:31 -0500

    add other packages

--- scripts/installDependencies.sh
+++ scripts/installDependencies.sh
@@ -7,7 +7,7 @@ apt-get update
 apt-get -y upgrade
 
 #install libs to compile 
-apt-get install -y i2c-tools libconfig++-dev libmysqlcppconn-dev libi2c-dev wiringpi  
+apt-get install -y i2c-tools libconfig++-dev libmysqlcppconn-dev libi2c-dev wiringpi  build-essential git-core
 
 #clean
 apt-get clean
