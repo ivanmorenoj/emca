d036ce439580e0cbdfc850ad84db7819f85b00c3
Ivan Moreno <ivan1697@gmail.com>
2019-07-15 12:45:53 -0500

    add release

---
+++ release/v1.0.0/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#installing runing dependencies
+echo "Installing dependencies"
+sh installRunDependencies.sh
+
+echo "Installing on /usr/bin/emca" 
+cp bin/emca /usr/bin/
+
+echo "Copy config.cfg on /etc/emca/config.cfg" 
+mkdir -p /etc/emca/
+cp config.cfg /etc/emca/config.cfg 
+
+echo "Creating /var/log/emca/ where is saved logfile"
+mkdir -p /var/log/emca/
+
+echo "Eopy emca.service on /etc/systemd/system/"
+cp emca.service /etc/systemd/system/
+
+echo "Enable emca service" 
+systemctl daemon-reload
+systemctl start emca
+systemctl enable emca
---
+++ release/v1.0.0/installRunDependencies.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# db user, pass and dbname
+DB_USER="insert_user"
+DB_PASS="inserp@ss"
+DB_NAME="EMCA"
+
+# load sql scripts
+SQL_SCRIPTS=$(find -name "*.sql")
+
+#update repo
+apt-get update
+
+#upgrade repos
+apt-get -y upgrade
+
+#install packages to run 
+apt-get install -y \
+    libconfig++-dev \
+    libmysqlcppconn-dev \
+    mariadb-server
+    #wiringpi \
+
+echo "Creating database $DB_NAME ."
+mysql -e "CREATE DATABASE $DB_NAME /*!40100 DEFAULT CHARACTER SET utf8 */;"
+echo "Creating user $DB_USER .."
+mysql -e "CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASS';"
+mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost';"
+mysql -e "FLUSH PRIVILEGES;"
+
+#creating tables in $DB_NAME
+for script in $SQL_SCRIPTS; do
+    echo ">>Executing script '$script' ..."
+    mysql $DB_NAME < $script
+done
+
+#clean
+apt-get clean
+
+echo 'Delete /var/lib/apt/lists/* /tmp/* /var/tmp/*' 
+rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
